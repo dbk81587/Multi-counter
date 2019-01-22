1351fb2cc894defdf9aa3efb8d42985746810713
Dongbi Kim <dbk81587@gmail.com>
2019-01-21 19:59:12 -0600

    Build and deployed

---
+++ build/precache-manifest.b9d95d668f8cdf0051c8be015ccfeec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3ca646acbdd22eeb1aa",
+    "url": "/static/js/main.c3ca646a.chunk.js"
+  },
+  {
+    "revision": "a10cfd385a42e856f57a",
+    "url": "/static/js/1.a10cfd38.chunk.js"
+  },
+  {
+    "revision": "c3ca646acbdd22eeb1aa",
+    "url": "/static/css/main.85fca8b4.chunk.css"
+  },
+  {
+    "revision": "bf8a16c8528a2fe2e63cf619e47ddce0",
+    "url": "/index.html"
+  }
+];
